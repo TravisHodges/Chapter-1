93f194bc0600e5cb665a62e8ff20dd6e2d4ab260
TravisHodges <37377833+TravisHodges@users.noreply.github.com>
2018-08-28 09:04:35 -0500

    Chapter 1 excercises

--- Excercises/src/chapter1excercises/FavoriteSong.java
+++ Excercises/src/chapter1excercises/FavoriteSong.java
@@ -4,7 +4,10 @@ public class FavoriteSong {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Baby what you wishin' for");
+		System.out.println("Maybe you should wish it more?");
+  		System.out.println("Maybe the world is yours?");
+		System.out.println("Maybe when it rains it pours?");
 	}
 
 }
