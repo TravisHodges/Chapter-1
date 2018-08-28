f49456a8e60cc2bcb34f20f7cbca56bbe7a0530c
TravisHodges <37377833+TravisHodges@users.noreply.github.com>
2018-08-28 09:39:24 -0500

    SammysMotto

---
+++ Excercises/src/caseproblems/SammysMotto.java
@@ -0,0 +1,10 @@
+package caseproblems;
+
+public class SammysMotto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("Sammmy's makes it fun in the sun.");
+	}
+
+}
---
+++ Excercises/src/caseproblems/SammysMotto2.java
@@ -0,0 +1,14 @@
+package caseproblems;
+
+public class SammysMotto2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
+     System.out.println("S                                  S");
+     System.out.println("S Sammy's makes it fun in the sun. S");
+     System.out.println("S                                  S");
+     System.out.println("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
+	}
+
+}
