11dfcbc346fa9f3070049048e80520f28faa6691
TravisHodges <37377833+TravisHodges@users.noreply.github.com>
2018-08-31 09:53:28 -0500

    ArithmeticDemo

--- Chapter2/src/youDoIt/FreeLance.java
+++ Chapter2/src/youDoIt/FreeLance.java
@@ -1,11 +1,30 @@
 package youDoIt;
 
+import java.util.Scanner;
+
 public class FreeLance {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	     String name;
-	     
+         int num1;
+         int num2;
+         int total;
+         
+         Scanner input = new  Scanner(System.in);
+         
+         System.out.println("What is your name? >> ");
+         name = input.next();
+         System.out.println("Please enter a number >> ");
+         num1 = input.nextInt();
+         System.out.println("Please enter a number >> ");
+         num2 = input.nextInt();
+         
+         total = num1 + num2;
+      
+         System.out.println("Thank you, " + name);
+         System.out.println("The answer is " + total);
+         
 	}
 
 }
